0fba888dd1e79158645cacd18b445a0743b8743f
baswa007 <laxman007kokle@gmail.com>
2026-03-01 17:53:10 +0530

    update

---
+++ src/main/java/com/example/demo/PipelineCheckController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class PipelineCheckController {
+
+    @GetMapping("/hello")
+    public Map<String, String> sayHello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "Success");
+        map.put("message", "Hello from CI/CD Pipeline!");
+        map.put("version", "1.0.0");
+        return map;
+    }
+}
